8f25cd2768461f918b6977c955d9391525ba411b
zhang.zhengming <zhang.zhengming@uneed.com>
2016-12-01 14:16:32 +0800

    updated

--- app/project/index.js
+++ app/project/index.js
@@ -1,9 +1,18 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createAction, createReducer } from 'redux-act';
-import store, { addReducer } from '../../store';
+import store, { addReducer } from '../store';
 import { registerPrimaryPanel } from '../workspace/primary';
 import ProjectPanel from './ui/ProjectPanel';
 import settings from '../settings';
+
+
+
+registerPrimaryPanel({
+  id: 'ProjectPanel',
+  displayName: 'Project Explorer',
+  component: ProjectPanel
+});
+
 
 const openProject = createAction('project.open');
 const reducer = createReducer({
@@ -18,12 +27,6 @@ const reducer = createReducer({
 });
 
 addReducer({ project: reducer });
-
-registerPrimaryPanel({
-  id: 'ProjectPanel',
-  displayName: 'Project Explorer',
-  component: ProjectPanel
-});
 
 settings.get('projectPath').then((path) => {
   if(!path) {
